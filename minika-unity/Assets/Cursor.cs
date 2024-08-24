e4342b95b3a4def198257cf6601d37ed307bc102
holistudio <daniel.bin.lu@gmail.com>
2024-08-24 12:53:18 -0400

    Move Cursor left or right on keypress
    Cursor moves left or right depending on arrow keys
    Cursor moves within bounds (min/max X)

--- minika-unity/Assets/Cursor.cs
+++ minika-unity/Assets/Cursor.cs
@@ -5,14 +5,64 @@ using UnityEngine;
 public class Cursor : MonoBehaviour
 {
     public GameObject currentShape;
+    public float xPosition;
+    private float testPosition;
     public float minPosition;
     public float maxPosition;
     public bool dropped;
-    
+    public float sensitivity;
+
     // Start is called before the first frame update
     void Start()
     {
+        xPosition = transform.position.x;
+        testPosition = transform.position.x;
+    }
+
+    bool checkInBounds(float testPosition)
+    {
+        if ((testPosition > minPosition) & (testPosition < maxPosition))
+        {
+            return true;
+        }
+        return false;
+    }
+    void moveCursor(string direction)
+    {
+
+        if (direction.Equals("left"))
+        {
+            testPosition = xPosition - sensitivity / 5f;
+        }
+        if (direction.Equals("right"))
+        {
+            testPosition = xPosition + sensitivity / 5f;
+        }
+
+        // check if next cursor position is within bounds
+        if(checkInBounds(testPosition))
+        {
+            // move cursor to next position
+            xPosition = testPosition;
+            transform.position = new Vector3(xPosition, transform.position.y, transform.position.z);
+        }
         
+    }
+
+    void OnGUI()
+    {
+        Event e = Event.current;
+        if (e.isKey)
+        {
+            if (e.keyCode == KeyCode.LeftArrow)
+            {
+                moveCursor("left");
+            }
+            if (e.keyCode == KeyCode.RightArrow)
+            {
+                moveCursor("right");
+            }
+        }
     }
 
     // Update is called once per frame
