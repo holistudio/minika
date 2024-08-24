ed5cd0fffd8a8859afc215dfd9efcd2018663c36
holistudio <daniel.bin.lu@gmail.com>
2024-08-24 15:09:57 -0400

    updateNextShape()
    Environment randomly chooses the next shape and sets that shape as the cursor's child.

--- minika-unity/Assets/Environment.cs
+++ minika-unity/Assets/Environment.cs
@@ -10,6 +10,7 @@ public class Environment : MonoBehaviour
     private string[] nextPossibleShapes = {"Cherry", "Strawberry", "Grapes", "Tangerine"};
     public bool gameOver = false;
 
+    public GameObject cursor;
     public GameObject possibleShapes;
     // Start is called before the first frame update
     void Start()
@@ -21,7 +22,35 @@ public class Environment : MonoBehaviour
     string updateNextShape()
     {
         int randomIndex = Random.Range(0, nextPossibleShapes.Length);
-        return nextPossibleShapes[randomIndex];
+        nextShape = nextPossibleShapes[randomIndex];
+        // find shape with name
+        Transform nextShapeTransform = possibleShapes.transform.Find(nextShape);
+
+        if (nextShapeTransform != null)
+        {
+            GameObject nextShapeObject = nextShapeTransform.gameObject;
+            // Duplicate the found object
+            GameObject duplicateObject = Instantiate(nextShapeObject);
+
+            Vector3 objectScale = duplicateObject.transform.localScale;
+
+            duplicateObject.transform.SetParent(cursor.transform);
+
+            // Set local scale
+            duplicateObject.transform.localScale = new Vector3(objectScale.x/cursor.transform.localScale.x,objectScale.y/cursor.transform.localScale.y,1);
+
+            // Set position
+            duplicateObject.transform.localPosition = Vector3.zero;
+
+            // Set as cursor's currentShape
+            cursor.GetComponent<Cursor>().currentShape = duplicateObject;
+        }
+        else
+        {
+            Debug.Log("Child object not found.");
+        }
+
+        return nextShape;
     }
 
     // Update is called once per frame
