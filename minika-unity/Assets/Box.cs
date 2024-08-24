03d4e36a50eeaac5dba43c0ce42e45eac284e6e2
holistudio <daniel.bin.lu@gmail.com>
2024-08-24 16:51:42 -0400

    Initial Box script

---
+++ minika-unity/Assets/Box.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void getShape (int shapeID)
+    {
+
+    }
+
+    bool checkSameShapesTouch()
+    {
+        return false;
+    }
+
+    void insertShape(string shapeName)
+    {
+
+    }
+
+    void mergeShape(GameObject shape1, GameObject shape2)
+    {
+
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
