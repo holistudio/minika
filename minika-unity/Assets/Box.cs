38bcfa0a0b64095f01025b6bab5bd418954c7b15
holistudio <daniel.bin.lu@gmail.com>
2024-08-24 17:05:52 -0400

    Code comments for merge shape algorithm

--- minika-unity/Assets/Box.cs
+++ minika-unity/Assets/Box.cs
@@ -15,24 +15,42 @@ public class Box : MonoBehaviour
 
     }
 
-    bool checkSameShapesTouch()
+    ArrayList checkSameShapesTouch(ArrayList shapesInBox)
     {
-        return false;
+        
+        ArrayList pairs = new ArrayList();
+        // go through the shapes in box
+
+        // return list of pairs of ids
+        return pairs;
     }
 
-    void insertShape(string shapeName)
+    void insertShape(string shapeName, Vector3 newPosition, int newID)
     {
-
+        // insert the shape inside the box at a specified position
     }
 
     void mergeShape(GameObject shape1, GameObject shape2)
     {
+        // get shape1's ID
+        
+        // compute the midpoint between the two shapes' centroids
+
+        // determine the next largest shape
+
+        // delete both shape1 and shape2
+
+        // insert new shape
 
     }
-    
+
     // Update is called once per frame
     void Update()
     {
-        
+        // check if the same shapes are touching
+        // and if so, get a list of ID pairs
+
+        // for each list of ID pairs
+        // merge shapes into the next largest shape
     }
 }
