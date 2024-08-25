771a3ddb7b1896458a78b3fa240a8254f8686e33
holistudio <daniel.bin.lu@gmail.com>
2024-08-24 21:00:48 -0400

    OverflowChecker collider/trigger

---
+++ minika-unity/Assets/OverflowChecker.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverflowChecker : MonoBehaviour
+{
+    public bool boxOverflow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
